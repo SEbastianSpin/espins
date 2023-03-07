1eb48d199f4d02b77f3280072c993884ef6663b1
SEbastianSpin <45522387+SEbastianSpin@users.noreply.github.com>
2023-03-07 06:08:27 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
 'use strict';
-console.log('Hello world');
-
-
-function stinger(string) {
-  const numToletter = {
-    "one": 1,
-    "two": 2,
-    "tree": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-    "zero": 0,
-   
-
-  }
-  if(string=="") return 0
-  if(numToletter[string])  return (numToletter[string])
-  return (string);
-}
-
-
-console.log(`${stinger("")}  ${stinger("two")}  ${stinger("tree")}  ${stinger("")}`)
-
-
-function converter (string) {
-  let numToarr=string.split(",");
+function converter(string) {
+  let numToarr = string.split(",");
   console.log(numToarr)
-  let f=numToarr.map(i=>stinger(i))
-  return f.filter((num)=>Number(num))
+  let f = numToarr.map(i => i == " " ? 0 : Number(i))
+  return f //.filter((num) => Number(num))
 }
 
+function summer(arr) {
 
-console.log(converter("one,two,tree"))
+  if(arr.find(e=>e<0))return('negative');
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+}
+
+function solver(s) { return summer(converter(s)) }
+
+console.log(solver("-1"))
+
+module.exports = {
+  solver: solver
+};
+
+
+
+
 
--- test.js
+++ test.js
@@ -1,79 +1,24 @@
 var assert = require('assert');
-
-
-
-function stinger(string) {
-  const numToletter = {
-    "one": 1,
-    "two": 2,
-    "tree": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-    "zero": 0,
-   
-
-  }
-  if(string=="") return 0
-  if(numToletter[string])  return (numToletter[string])
-  return (string);
-}
-
-
-
-// 
-
-
-
-function converter (string) {
-  let numToarr=[...string]
-  numToarr.map((i)=>{
-    stinger(i)
-  })
- return numToarr
-}
-
-
-function converter (string) {
-  let numToarr=string.split(",");
-  console.log(numToarr)
-  let f=numToarr.map(i=>Number(i))
-  return f.filter((num)=>Number(num))
-}
-
-function summer (arr) {
-  return arr.reduce((accumulator, currentValue) => accumulator + currentValue)
-}
-
-
-
-
+const fun = require('./index.js');
 
 
 
 describe('Test Suite 1', function() {
 
 
-  it('Test ONE', function() {
-    assert.ok(1 === stinger("one"), "1 ===' 1");
-    assert.ok(true, "This should fail");
-  });
-
-  it('Test "empty" ', function() {
-    assert.ok(0 === stinger(""), "0 ===' empty");
-    assert.ok(true, "This should fail");
-  });
 
   it('Test sum ', function() {
-    assert.ok(6 === summer(converter("1,2,3")), "0 ===' empty");
-    assert.ok(true, "This should fail");
+    assert.ok(6 === fun.solver("1,2,3"), "0 ===' empty");
+    assert.ok(true, "su, 1+2+3=6");
   });
 
+  it('Test empty eq 0 ', function() {
+    assert.ok(0 === (fun.solver(" ")), "0 === empty");
+    assert.ok(true, "0 es empty string");
+  });
 
+  it('Test empty eq 0 ', function() {
+    assert.ok("negative"=== (fun.solver("-1,-1 ")), "neg");
+  });
 });
 
-
-"one"
